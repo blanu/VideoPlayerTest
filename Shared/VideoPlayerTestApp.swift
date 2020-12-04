b5fda43b927190935b3159b94ecf8731c0ab7bfe
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2020-12-03 19:48:55 -0600

    Initial Commit

---
+++ Shared/VideoPlayerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerTestApp.swift
+//  Shared
+//
+//  Created by Dr. Brandon Wiley on 12/3/20.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
